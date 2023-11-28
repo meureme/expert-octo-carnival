be3b6afdf42de93ccfd4e17d211386b731dd5c00
meureme <nikoladzeniko019@gmail.com>
2023-11-28 14:25:24 +0000

    Fixing readily

---
+++ 28112023_142524_readily_ano0n.sh
@@ -0,0 +1,1 @@
+In 1631, two London bible printers accidentally left the word "not" out of the seventh commandment, which then read, "Thou shalt commit adultery." This legendary book is now known as the "Wicked Bible."
